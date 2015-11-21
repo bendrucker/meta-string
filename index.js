a71a6236ca2ba6b3909e0d302dfab431a14479aa
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2015-11-21 14:29:41 -0500

    Support spaces in metadata values

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function metaString (value) {
   if (typeof value !== 'object') return String(value)
   if (Array.isArray(value)) return value.join(', ')
   return metaString(Object.keys(value).map(function keyVal (key) {
-    return [toDash(key), value[key]].join('=')
+    return [toDash(key), '"' + value[key] + '"'].join('=')
   }))
 }
